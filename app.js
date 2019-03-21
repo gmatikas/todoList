fb45c484932ae1b6e9005f9626c86d215b4e1402
George Matikas <g_matikas@hotmail.com>
2019-03-21 16:30:51 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -1,63 +1,67 @@
-function addItem() {
-	const input = document.getElementById('newItem').value;
+function handle(e) {
+	const t = e.target;
+	if (t.tagName === 'A') moveItem(t);
+	else deleteItem(t);
+}
 
-	if (input === '')
-		document.getElementById('error').innerText = 'This cannot be empty';
+function deleteItem(item) {
+	const parent = item.parentNode;
+	parent.parentNode.removeChild(parent);
+}
+
+function moveItem(oldItem) {
+	const newItem = document.createElement('a');
+	newItem.href = '#';
+	newItem.addEventListener('click', (e) => { handle(e); }, false);
+
+	const text = oldItem.innerText;
+	const textNode = document.createTextNode(text);
+	newItem.appendChild(textNode);
+
+	const deleteIcon = document.createElement('span');
+	deleteIcon.className = 'fa fa-remove text-danger';
+	newItem.appendChild(deleteIcon);
+
+	const oldList = oldItem.parentNode.id;
+
+	if (oldList === 'todo') {
+		const newList = document.getElementById('completed');
+		newItem.className = 'list-group-item list-group-item-action list-group-item-success d-flex justify-content-between align-items-center';
+		newList.insertBefore(newItem, newList.firstChild);
+	} else {
+		const newList = document.getElementById('todo');
+		newItem.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
+		newList.insertBefore(newItem, newList.firstChild);
+	}
+
+	oldItem.parentNode.removeChild(oldItem);
+}
+
+function addItem() {
+	const text = document.getElementById('newItem').value;
+
+	if (text === '')
+		document.getElementById('error').innerText = 'This field cannot be empty';
 	else {
-		const inputText = document.createTextNode(input);
 		document.getElementById('error').innerText = '';
 
-		const item = document.createElement('a');
-		item.href = '#';
-		item.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
-		item.appendChild(inputText);
+		const newItem = document.createElement('a');
+		newItem.href = '#';
+		newItem.addEventListener('click', (e) => { handle(e); }, false);
+		newItem.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
 
-		const del = document.createElement('span');
-		del.className = 'fa fa-remove text-danger';
-		item.appendChild(del);
+		const textNode = document.createTextNode(text);
+		newItem.appendChild(textNode);
 
-		item.addEventListener('click', (e) => { handle(e); }, false);
+		const deleteIcon = document.createElement('span');
+		deleteIcon.className = 'fa fa-remove text-danger';
+		newItem.appendChild(deleteIcon);
 
 		const todoList = document.getElementById('todo');
-		todoList.insertBefore(item, todoList.firstChild);
+		todoList.insertBefore(newItem, todoList.firstChild);
 		document.getElementById('newItem').value = '';
 	}
 }
 
-function handle(e) {
-	if (e.target.tagName === 'A') moveItem(e.target);
-	else deleteItem(e.target);
-}
-
-function deleteItem(item) {
-	item.parentNode.parentNode.removeChild(item.parentNode);
-}
-
-function moveItem(old) {
-	const value = old.innerText;
-	const text = document.createTextNode(value);
-
-	const item = document.createElement('a');
-	item.href = '#';
-	item.appendChild(text);
-
-	const del = document.createElement('span');
-	del.className = 'fa fa-remove text-danger';
-	item.appendChild(del);
-
-	item.addEventListener('click', (e) => { handle(e); }, false);
-
-	const list = old.parentNode.id;
-
-	if (list === 'todo') {
-		const compList = document.getElementById('completed');
-		item.className = 'list-group-item list-group-item-action list-group-item-success d-flex justify-content-between align-items-center';
-		compList.insertBefore(item, compList.firstChild);
-	} else {
-		const todoList = document.getElementById('todo');
-		item.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
-		todoList.insertBefore(item, todoList.firstChild);
-	}
-
-	old.parentNode.removeChild(old);
-}
+const addButton = document.getElementById('add');
+addButton.addEventListener('click', () => { addItem(); }, false);
