ae8de735a0940de5a58ceceabaeab4b4942f521f
George Matikas <g_matikas@hotmail.com>
2019-03-22 17:27:43 +0200

    IndexedDB Implementation

--- app.js
+++ app.js
@@ -1,64 +1,192 @@
+let db;
+
 window.onload = () => {
 	const addButton = document.getElementById('add');
-	addButton.addEventListener('click', () => { addItem(); }, false);
+	addButton.addEventListener('click', () => {
+		addItem();
+	}, false);
+
+	const request = window.indexedDB.open('ToDo', 1);
+
+	request.onerror = () => {
+		throw new Error('Database failed to open');
+	};
+
+	request.onsuccess = () => {
+		db = request.result;
+
+		displayData();
+	};
+
+	request.onupgradeneeded = (e) => {
+		db = e.target.result;
+
+		const objectStore = db.createObjectStore('items', {
+			keyPath: 'id',
+			autoIncrement: true,
+		});
+
+		objectStore.createIndex('value', 'value', {
+			unique: false,
+		});
+		objectStore.createIndex('status', 'status', {
+			unique: false,
+		});
+	};
 };
+
+function displayData() {
+	const transaction = db.transaction(['items'], 'readwrite');
+	const objectStore = transaction.objectStore('items');
+
+	objectStore.openCursor().onsuccess = (e) => {
+		const cursor = e.target.result;
+
+		if (cursor) {
+			renderItem(cursor.value);
+			cursor.continue();
+		}
+	};
+}
 
 function handle(e) {
 	const t = e.target;
 
 	if (t.tagName === 'A') moveItem(t);
-	else deleteItem(t.parentNode);
+	else {
+		deleteItem(t.parentNode);
+		deleteData(t.parentNode);
+	}
+}
+
+function deleteData(item) {
+	const id = Number(item.getAttribute('item-id'));
+
+	const transaction = db.transaction(['items'], 'readwrite');
+	const objectStore = transaction.objectStore('items');
+	objectStore.delete(id);
+
+	transaction.onerror = () => {
+		throw new Error('Database transaction failed');
+	};
 }
 
 function deleteItem(item) {
 	item.parentNode.removeChild(item);
 }
 
+function moveData(item) {
+	const id = Number(item.getAttribute('item-id'));
+
+	const transaction = db.transaction(['items'], 'readwrite');
+	const objectStore = transaction.objectStore('items');
+	const request = objectStore.get(id);
+
+	request.onsuccess = () => {
+		const oldItem = request.result;
+
+		oldItem.status = (oldItem.status + 1) % 2;
+
+		objectStore.put(oldItem);
+
+		renderItem(oldItem);
+	};
+
+	transaction.onerror = () => {
+		throw new Error('Database transaction failed');
+	};
+}
+
 function moveItem(oldItem) {
-	const text = oldItem.innerText;
-	const stat = Number(oldItem.parentNode.id);
-
+	moveData(oldItem);
 	deleteItem(oldItem);
+}
 
-	const newItem = { value: text, status: (stat + 1) % 2 };
-	renderItem(newItem);
+function addData(text) {
+	const transaction = db.transaction(['items'], 'readwrite');
+	const objectStore = transaction.objectStore('items');
+
+	const tmpItem = {
+		value: text,
+		status: 0,
+	};
+	const request = objectStore.add(tmpItem);
+
+	request.onsuccess = (e) => {
+		const newItem = {
+			id: e.target.result,
+			value: text,
+			status: 0,
+		};
+		renderItem(newItem);
+	};
+
+	transaction.onerror = () => {
+		throw new Error('Database transaction failed');
+	};
+}
+
+/**
+ * Whether string has value.
+ *
+ * @return {bool}
+ */
+function IsEmpty(text) {
+	return !(text.trim());
 }
 
 function addItem() {
 	const text = document.getElementById('newItem').value;
 
-	if (text === '')
+	if (IsEmpty(text)) {
 		document.getElementById('error').innerText = 'This field cannot be empty';
-	else {
-		document.getElementById('error').innerText = '';
-		document.getElementById('newItem').value = '';
-
-		const newItem = { value: text, status: 0 };
-		renderItem(newItem);
+		return;
 	}
+
+	document.getElementById('error').innerText = '';
+	document.getElementById('newItem').value = '';
+
+	addData(text);
+}
+
+/**
+ * Whether a task has been completed
+ *
+ * @param {string} status
+ * @return {bool}
+ */
+function IsCompleted(status) {
+	if (status === 0) return false;
+
+	return true;
 }
 
 function renderItem(item) {
-	const text = item.value;
-	const stat = item.status;
+	const itemId = item.id;
+	const itemText = item.value;
+	const itemStatus = item.status;
 
 	const newItem = document.createElement('a');
 	newItem.href = '#';
-	newItem.addEventListener('click', (e) => { handle(e); }, false);
+	newItem.setAttribute('item-id', itemId);
+	newItem.addEventListener('click', (e) => {
+		handle(e);
+	}, false);
 
-	const textNode = document.createTextNode(text);
+	const textNode = document.createTextNode(itemText);
 	newItem.appendChild(textNode);
 
 	const deleteIcon = document.createElement('span');
 	deleteIcon.className = 'fa fa-remove text-danger';
 	newItem.appendChild(deleteIcon);
 
-	if (stat === 0)
+	if (IsCompleted(itemStatus)) {
 		newItem.className = 'list-group-item list-group-item-action list-group-item-success d-flex justify-content-between align-items-center';
-	else
+	} else {
 		newItem.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
+	}
 
-	const list = document.getElementById(stat);
+	const list = document.getElementById(Number(itemStatus));
 
 	list.insertBefore(newItem, list.firstChild);
 }
