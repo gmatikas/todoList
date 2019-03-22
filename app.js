931929c4a84017132b1f68af9c4565c7a98353a3
George Matikas <g_matikas@hotmail.com>
2019-03-22 10:45:27 +0200

    Separated render logic

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
+window.onload = () => {
+	const addButton = document.getElementById('add');
+	addButton.addEventListener('click', () => { addItem(); }, false);
+};
+
 function handle(e) {
 	const t = e.target;
+
 	if (t.tagName === 'A') moveItem(t);
-	else deleteItem(t);
+	else deleteItem(t.parentNode);
 }
 
 function deleteItem(item) {
-	const parent = item.parentNode;
-	parent.parentNode.removeChild(parent);
+	item.parentNode.removeChild(item);
 }
 
 function moveItem(oldItem) {
-	const newItem = document.createElement('a');
-	newItem.href = '#';
-	newItem.addEventListener('click', (e) => { handle(e); }, false);
-
 	const text = oldItem.innerText;
-	const textNode = document.createTextNode(text);
-	newItem.appendChild(textNode);
+	const stat = Number(oldItem.parentNode.id);
 
-	const deleteIcon = document.createElement('span');
-	deleteIcon.className = 'fa fa-remove text-danger';
-	newItem.appendChild(deleteIcon);
+	deleteItem(oldItem);
 
-	const oldList = oldItem.parentNode.id;
-
-	if (oldList === 'todo') {
-		const newList = document.getElementById('completed');
-		newItem.className = 'list-group-item list-group-item-action list-group-item-success d-flex justify-content-between align-items-center';
-		newList.insertBefore(newItem, newList.firstChild);
-	} else {
-		const newList = document.getElementById('todo');
-		newItem.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
-		newList.insertBefore(newItem, newList.firstChild);
-	}
-
-	oldItem.parentNode.removeChild(oldItem);
+	const newItem = { value: text, status: (stat + 1) % 2 };
+	renderItem(newItem);
 }
 
 function addItem() {
@@ -44,24 +31,34 @@ function addItem() {
 		document.getElementById('error').innerText = 'This field cannot be empty';
 	else {
 		document.getElementById('error').innerText = '';
-
-		const newItem = document.createElement('a');
-		newItem.href = '#';
-		newItem.addEventListener('click', (e) => { handle(e); }, false);
-		newItem.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
-
-		const textNode = document.createTextNode(text);
-		newItem.appendChild(textNode);
-
-		const deleteIcon = document.createElement('span');
-		deleteIcon.className = 'fa fa-remove text-danger';
-		newItem.appendChild(deleteIcon);
-
-		const todoList = document.getElementById('todo');
-		todoList.insertBefore(newItem, todoList.firstChild);
 		document.getElementById('newItem').value = '';
+
+		const newItem = { value: text, status: 0 };
+		renderItem(newItem);
 	}
 }
 
-const addButton = document.getElementById('add');
-addButton.addEventListener('click', () => { addItem(); }, false);
+function renderItem(item) {
+	const text = item.value;
+	const stat = item.status;
+
+	const newItem = document.createElement('a');
+	newItem.href = '#';
+	newItem.addEventListener('click', (e) => { handle(e); }, false);
+
+	const textNode = document.createTextNode(text);
+	newItem.appendChild(textNode);
+
+	const deleteIcon = document.createElement('span');
+	deleteIcon.className = 'fa fa-remove text-danger';
+	newItem.appendChild(deleteIcon);
+
+	if (stat === 0)
+		newItem.className = 'list-group-item list-group-item-action list-group-item-success d-flex justify-content-between align-items-center';
+	else
+		newItem.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
+
+	const list = document.getElementById(stat);
+
+	list.insertBefore(newItem, list.firstChild);
+}
